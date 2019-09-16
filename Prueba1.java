42de3fce2adf3b8c435bc59a582f183e571d91c4
Eduardo <38383695+eudrad@users.noreply.github.com>
2019-09-15 21:51:48 -0500

    Add files via upload

---
+++ Prueba1.java
@@ -0,0 +1,8 @@
+package prueba1;
+import java.io.IOException;
+public class Prueba1 {
+    public static void main(String[] args) throws IOException {
+        Ventana frm = new Ventana();
+        frm.setVisible(true);        
+    }
+}
